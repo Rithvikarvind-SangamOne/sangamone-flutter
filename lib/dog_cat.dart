2267457079d14c0819f8708170496cf123c8256a
Rithvik <rithvikarvind.sangamone@gmail.com>
2022-01-26 14:55:45 +0530

    Fourth Commit

--- lib/dog_cat.dart
+++ lib/dog_cat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class Sound extends StatefulWidget {
   const Sound({Key? key}) : super(key: key);
@@ -8,6 +9,19 @@ class Sound extends StatefulWidget {
 }
 
 class _SoundState extends State<Sound> {
+  late AudioPlayer player;
+  @override
+  void initState() {
+    super.initState();
+    player=AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,14 +34,20 @@ class _SoundState extends State<Sound> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: FlatButton(
-                onPressed: (){},
+                onPressed: () async{
+                  await player.setAsset('assets/audio/Dog-barking.mp3');
+                  player.play();
+                },
                   child: Image(image: AssetImage('assets/Dog.jpeg'),)),
             ),
             SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: FlatButton(
-                onPressed: () {},
+                onPressed: () async{
+                  await player.setAsset('assets/audio/Cat-meow.mp3');
+                  player.play();
+                },
                   child: Image(image: AssetImage('assets/Cat.jpeg'),)),
             ),
           ],
